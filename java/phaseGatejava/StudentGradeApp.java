c6d5652dfdf56d1eb556ffb00cd7cfa4c152c547
Abdulazeez20012 <abdulazeezmuhammad855@gmail.com>
2024-12-03 11:47:52 +0100

    update task1

--- java/phaseGatejava/StudentGradeApp.java
+++ java/phaseGatejava/StudentGradeApp.java
@@ -11,19 +11,21 @@ int numberSubject = scanner.nextInt();
 
 int[][] scores = new int[numberStudent][numberSubject];
 
-        for (int i = 0; i < numberStudent; i++) {
-            System.out.println("Enter scores for student " + (i + 1) + ":");
-
-
+        for (int a = 0; a < numberStudent; a++) {
+            System.out.println("Enter scores for student " + (a + 1) + ":");
+		for (int b = 0; b < numberSubject; b++){
+		     int score;
+			do {
+			     System.out.print("Enter score for subject" + (b + 1) + " (0-100) :");
+                    score = scanner.nextInt();
+                    if (score < 0 || score > 100) {
+                          System.out.println(" Please enter a score between 0 and 100.");
+                    }
+                } while (score < 0 || score > 100);
+                scores[a][b] = score;
+            }
+        }
+}
 
 }
 
-
-
-
-
-
- }
-
-
-}
