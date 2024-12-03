b04b90f81c6ab0c6a14cb0e20bdd2f43cf14b7f6
Abdulazeez20012 <abdulazeezmuhammad855@gmail.com>
2024-12-03 14:56:45 +0100

    update2task1

--- java/phaseGatejava/StudentGradeApp.java
+++ java/phaseGatejava/StudentGradeApp.java
@@ -3,29 +3,49 @@ import java.util.Scanner;
       public static void main (String[] args) {
           Scanner scanner = new Scanner(System.in);
 
-System.out.print("Enter the number of student: ");
+System.out.print("How many student do you have? : ");
 int numberStudent = scanner.nextInt();
 
-System.out.print("Enter the number of subject: ");
+System.out.print("How many subject do you offer? : ");
 int numberSubject = scanner.nextInt();
 
-int[][] scores = new int[numberStudent][numberSubject];
+System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+System.out.println("Saved Successfully");
 
+int[][] scores = new int[numberStudent][numberSubject];
+int[] totals = new int [numberStudent];
+double[] averages = new double[numberStudent];
+         
+
+		int totalScore = 0;
         for (int a = 0; a < numberStudent; a++) {
-            System.out.println("Enter scores for student " + (a + 1) + ":");
+		
+            System.out.println("\nEnter scores for student "  +  (a + 1)+":");
 		for (int b = 0; b < numberSubject; b++){
 		     int score;
 			do {
-			     System.out.print("Enter score for subject" + (b + 1) + " (0-100) :");
+			     System.out.println("Enter score for subject" + (b + 1)+":");
                     score = scanner.nextInt();
+			     System.out.println("=================================================================");
                     if (score < 0 || score > 100) {
                           System.out.println(" Please enter a score between 0 and 100.");
                     }
-                } while (score < 0 || score > 100);
-                scores[a][b] = score;
-            }
+                       } while (score < 0 || score > 100);
+                         scores[a][b] = score; 
+			 totals += score;
+    			int arrayLength = score.lenght;
+			averages = (totals / (double)arrayLength);
+
+
+			System.out.println(" totals = " + score);
+			System.out.println("averages = " + averages);
+           		
+      					 
+        }
+
+
+
         }
 }
-
+     			 
 }
-
